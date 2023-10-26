8b4a5902ffcc77a646d49b2c4900fe0e3eccf0b6
parad83 <paradzinskiadrian@gmail.com>
2023-10-26 18:39:50 +0200

    fix last commit

--- src/components/Simulation.tsx
+++ src/components/Simulation.tsx
@@ -1,10 +1,5 @@
 import React, { useState } from "react";
 
-interface Intervals {
-  xIntervals: React.ReactNode[];
-  yIntervals: React.ReactNode[];
-}
-
 const Simulation = () => {
   const windowSize = { width: 800, height: 800 };
 
@@ -19,206 +14,112 @@ const Simulation = () => {
 
   const [dragInitPos, setDragInitPos] = useState({ x: 0, y: 0 });
 
-  const [axisIntervals, setAxisIntervals] = useState(10);
+  const [axisIntervals, setAxisIntervals] = useState(9);
+  const intervalsArray = Array(axisIntervals);
+  const interval = windowSize.width / (axisIntervals + 1);
 
-  const intervals: Intervals = {
-    xIntervals: [],
-    yIntervals: [],
+  for (let i = 0; i < axisIntervals / 2; i++) {
+    intervalsArray[i] = interval * (i + 1);
+    intervalsArray[axisIntervals - i] = interval * (axisIntervals - i);
+  }
+
+  const handleAxisIntervals = (event) => {
+    setAxisIntervals(Number(event.target.value));
   };
 
-  for (let i = 1; i < axisIntervals - 1; i++) {
-    const xPosition = ((coordinateSystem.x * 2) / axisIntervals) * (i + 1);
-    const yPosition = ((coordinateSystem.y * 2) / axisIntervals) * (i + 1);
+  const handleMouseDown = (event) => {
+    setIsDragging(true);
+    setDragInitPos({ x: event.clientX, y: event.clientY });
+  };
 
-    if (xPosition - coordinateSystem.x !== 0) {
-      intervals.xIntervals.push(
-        <div
-          key={`x-${i}`}
-          className="axis-interval"
-          style={{
-            top: -3,
-            left: xPosition,
-          }}
-        >
-          {" "}
-          <div className="axis-interval-label">
-            {Number(xPosition - coordinateSystem.x).toFixed(2)}
-          </div>
-        </div>
-      );
-    }
-
-    if (yPosition - coordinateSystem.y !== 0) {
-      intervals.yIntervals.push(
-        <div
-          key={`y-${i}`}
-          className="axis-interval"
-          style={{
-            left: -3,
-            top: yPosition,
-          }}
-        >
-          {" "}
-          <div className="axis-interval-label">
-            {Number(yPosition - coordinateSystem.y).toFixed(2)}
-          </div>
-        </div>
-      );
-    }
-
-    //   const intervals = { x: [], y: [] };
-
-    //   for (let i = 1; i < axisIntervals - 1; i++) {
-    //     const xPosition = ((coordinateSystem.x * 2) / axisIntervals) * (i + 1);
-    //     const yPosition = ((coordinateSystem.x * 2) / axisIntervals) * (i + 1);
-    //     const xIntervals =
-    //       xPosition - coordinateSystem.x === 0 ? (
-    //         <div></div>
-    //       ) : (
-    //         <div
-    //           key={`x-${i}`}
-    //           className="axis-interval"
-    //           style={{
-    //             top: -3,
-    //             left: xPosition,
-    //           }}
-    //         >
-    //           {" "}
-    //           <div className="axis-interval-label">
-    //             {Number(xPosition - coordinateSystem.x).toFixed(2)}
-    //           </div>
-    //         </div>
-    //       );
-    //     const yIntervals =
-    //       yPosition - coordinateSystem.y === 0 ? null : (
-    //         <div
-    //           key={`y-${i}`}
-    //           className="axis-interval"
-    //           style={{
-    //             top: -3,
-    //             left: yPosition,
-    //           }}
-    //         >
-    //           {" "}
-    //           <div className="axis-interval-label">
-    //             {Number(yPosition - coordinateSystem.x).toFixed(2)}
-    //           </div>
-    //         </div>
-    //       );
-    //     intervals.x.push(xIntervals);
-    //     intervals.y.push(yIntervals);
-    //   }
-
-    //   const xIntervals = Array.from({ length: axisIntervals - 1 }, (_, i) => {
-    //     const position = ((coordinateSystem.x * 2) / axisIntervals) * (i + 1);
-    //     return position - coordinateSystem.x === 0 ? null : (
-    //       <div
-    //         key={i}
-    //         className="axis-interval"
-    //         style={{
-    //           top: -3,
-    //           left: position,
-    //         }}
-    //       >
-    //         {" "}
-    //         <div className="axis-interval-label">
-    //           {Number(position - coordinateSystem.x).toFixed(2)}
-    //         </div>
-    //       </div>
-    //     );
-    //   });
-    //   const yIntervals = Array.from({ length: axisIntervals - 1 }, (_, i) => {
-    //     const position = ((coordinateSystem.y * 2) / axisIntervals) * (i + 1);
-    //     // const roundedPosition = Number(position.toFixed(2));
-    //     return position - coordinateSystem.y === 0 ? null : (
-    //       <div
-    //         key={i}
-    //         className="axis-interval"
-    //         style={{
-    //           left: -3,
-    //           top: position,
-    //         }}
-    //       >
-    //         {" "}
-    //         <div className="axis-interval-label">
-    //           {Number(position - coordinateSystem.y).toFixed(2)}
-    //         </div>
-    //       </div>
-    //     );
-    //   });
-
-    const handleAxisIntervals = (event) => {
-      setAxisIntervals(Number(event.target.value));
-    };
-
-    const handleMouseDown = (event) => {
-      setIsDragging(true);
-      setDragInitPos({ x: event.clientX, y: event.clientY });
-    };
-
-    const handleMouseMove = (event) => {
-      if (isDragging) {
-        const x = event.clientX;
-        const y = event.clientY;
-
-        const offsetX = dragInitPos.x - x;
-        const offsetY = dragInitPos.y - y;
-        setCoordinateSystem({
-          x: coordinateSystem.x - offsetX * 0.1,
-          y: coordinateSystem.y - offsetY * 0.1,
-        });
-      }
-    };
-
-    const handleMouseUp = (event) => {
-      setIsDragging(false);
-    };
-
+  const handleMouseMove = (event) => {
     if (isDragging) {
-      document.body.style.cursor = "pointer";
-    } else {
-      document.body.style.cursor = "default";
-    }
+      const x = event.clientX;
+      const y = event.clientY;
 
-    return (
-      <>
-        <div
-          className="simulation-window"
-          onMouseMove={handleMouseMove}
-          onMouseUp={handleMouseUp}
-          onMouseDown={handleMouseDown}
-          style={{ width: windowSize.width, height: windowSize.height }}
-        >
-          <div className="axis x-axis" style={{ top: coordinateSystem.y }}>
-            x-axis
-            {intervals.xIntervals}
-          </div>
-          <div className="axis y-axis" style={{ left: coordinateSystem.x }}>
-            y-axis
-            {intervals.yIntervals}
-          </div>
-          {/* <div
+      const offsetX = dragInitPos.x - x;
+      const offsetY = dragInitPos.y - y;
+      setCoordinateSystem({
+        x: coordinateSystem.x - offsetX * 0.1,
+        y: coordinateSystem.y - offsetY * 0.1,
+      });
+    }
+  };
+
+  const handleMouseUp = (event) => {
+    setIsDragging(false);
+  };
+
+  if (isDragging) {
+    document.body.style.cursor = "pointer";
+  } else {
+    document.body.style.cursor = "default";
+  }
+
+  return (
+    <>
+      <div
+        className="simulation-window"
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+        onMouseDown={handleMouseDown}
+        style={{ width: windowSize.width, height: windowSize.height }}
+      >
+        <div className="axis x-axis" style={{ top: coordinateSystem.y }}>
+          {intervalsArray.map((interval, i) => (
+            <div
+              key={`y-${i}`}
+              className="axis-interval"
+              style={{
+                top: -3,
+                left: interval,
+              }}
+            >
+              {" "}
+              <div className="axis-interval-label">
+                {Number(interval - coordinateSystem.x).toFixed(2)}
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="axis y-axis" style={{ left: coordinateSystem.x }}>
+          {intervalsArray.map((interval, i) => (
+            <div
+              key={`y-${i}`}
+              className="axis-interval"
+              style={{
+                left: -3,
+                top: interval,
+              }}
+            >
+              {" "}
+              <div className="axis-interval-label">
+                {Number(interval - coordinateSystem.y).toFixed(2)}
+              </div>
+            </div>
+          ))}
+        </div>
+        {/* <div
         className="axis-0"
         style={{ top: coordinateSystem.y - 2, left: coordinateSystem.x - 2 }}
       ></div> */}
-        </div>
-        <div className="simulation-settings">
-          {" "}
-          <label htmlFor="#axis-intervals">Axis intervals</label>
-          <input
-            id="axis-intervals"
-            type="number"
-            value={axisIntervals}
-            onChange={handleAxisIntervals}
-            min="0"
-          />
-          <button onChange={() => setCoordinateSystem(defaultCoSystem)}>
-            Center axis
-          </button>
-        </div>
-      </>
-    );
-  }
+      </div>
+      <div className="simulation-settings">
+        {" "}
+        <label htmlFor="#axis-intervals">Axis intervals</label>
+        <input
+          id="axis-intervals"
+          type="number"
+          value={axisIntervals}
+          onChange={handleAxisIntervals}
+          min="0"
+        />
+        <button onClick={() => setCoordinateSystem(defaultCoSystem)}>
+          Center axis
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Simulation;
